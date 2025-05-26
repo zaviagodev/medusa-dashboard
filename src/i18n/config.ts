a25b4a3c3004b8126404766d94aeb8f58c4998ca
umer2001 <umer2001.uf@gmail.com>
2025-05-26 15:12:53 +0700

    chnage: fallback language from English to Thai in i18n configuration

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -10,7 +10,7 @@ export const defaultI18nOptions: InitOptions = {
     lookupLocalStorage: "lng",
     order: ["cookie", "localStorage", "header"],
   },
-  fallbackLng: "en",
+  fallbackLng: "th",
   interpolation: {
     escapeValue: false,
   },
